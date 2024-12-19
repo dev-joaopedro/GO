4f7db8689334d56505888d76d7b5e6a2746e3389
dev-joaopedro <jjooaaoo46@gmail.com>
2024-12-19 09:56:44 -0300

    3

---
+++ pratica_slices/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	var tarefas []string
+
+	// Adicionar itens no slice
+
+	tarefas = append(tarefas, "Estudar GO")
+	tarefas = append(tarefas, "Seguir o canal do Johny")
+	tarefas = append(tarefas, "Ler um livro")
+
+	fmt.Println("Tarefas:", tarefas)
+	fmt.Println("Tamanho do slice atual:", len(tarefas))
+
+	// Slicing / Cortar
+
+	//tarefas = tarefas[1:]
+
+	//fmt.Println("Removendo a 1 tarefa:", tarefas)
+
+	//tarefas = tarefas[:len(tarefas) - 1]
+	//fmt.Println("Removendo ultimo item:", tarefas)
+
+	tarefas = append(tarefas[:1], tarefas[2:]...)
+	fmt.Println("Remover o segundo item:", tarefas)
+}
--- slices/main.go
+++ slices/main.go
@@ -6,21 +6,45 @@ import "fmt"
 
 func main() {
 
-	var lista []int //Declarando slices de inteiro
+	//var lista []int //Declarando slices de inteiro
 
-	fmt.Println("Slice vazio", lista)
+	//fmt.Println("Slice vazio", lista)
 
-	lista = append(lista, 10, 50, 30, 20)
+	//lista = append(lista, 10, 50, 30, 20)
 
-	fmt.Println("Slice com valores:", lista)
+	//fmt.Println("Slice com valores:", lista)
+	//fmt.Println("Pegando slice na posiçao 0:", lista[0])
 
-	myArray := [7]int{1,2,3,4,5,6,7}
+	//myArray := [7]int{1,2,3,4,5,6,7}
 
-	fmt.Println("Meu array:", myArray)
+	//fmt.Println("Meu array:", myArray)
 
 	//Criar um slice a partir de um array existente.
 
-	mySlice := myArray[1:5] // Criar um slice incluindo elementos do incide 1 a 4
+	//mySlice := myArray[1:5] // Criar um slice incluindo elementos do incide 1 a 4
 
-	fmt.Println("Slice a partir do Array:", mySlice)
+	//fmt.Println("Slice a partir do Array:", mySlice)
+
+	var numeros []int
+
+	numeros = append(numeros, 50, 10, 99, 30)
+
+	fmt.Println("Conteudo do slice numeros:", numeros)
+	fmt.Println("Pegando posição 0 do slice:", numeros[0])
+
+	frutas := []string{"banana", "maça", "mamao"}
+
+	fmt.Println("Slice de frutas:", frutas)
+
+	frutas = append(frutas, "pera")
+
+	fmt.Println("Slice de frutas após o append:", frutas)
+
+	nomes := make([]string, 0)
+
+	fmt.Println("Slice nomes:", nomes)
+
+	nomes = append(nomes, "Matheus", "João")
+
+	fmt.Println("Slice de nomes:", nomes)
 }
